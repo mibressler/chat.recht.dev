06c66ec19eb2066c95d9af191a9c0f34222a1810
mibressler <53796824+mibressler@users.noreply.github.com>
2023-01-02 22:17:35 +0100

    ui cleanup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,7 @@
 <template>
   <v-app id="inspire">
     <v-system-bar app>
-      <v-spacer></v-spacer>
-
-      <v-icon>mdi-square</v-icon>
-
-      <v-icon>mdi-circle</v-icon>
-
-      <v-icon>mdi-triangle</v-icon>
+      
     </v-system-bar>
 
     <v-app-bar
@@ -18,63 +12,10 @@
     >
       <v-spacer></v-spacer>
 
-      <v-responsive max-width="156">
-        <v-text-field
-          dense
-          flat
-          hide-details
-          rounded
-          solo-inverted
-        ></v-text-field>
-      </v-responsive>
+
     </v-app-bar>
 
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      width="300"
-    >
-
-
-      <v-sheet
-        color="grey lighten-5"
-        height="128"
-        width="100%"
-      ></v-sheet>
-
-      <v-list
-        class="pl-14"
-        shaped
-      >
-        <v-list-item
-          v-for="n in 5"
-          :key="n"
-          link
-        >
-          <v-list-item-content>
-            <v-list-item-title>Item {{ n }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-navigation-drawer
-      app
-      clipped
-      right
-    >
-      <v-list>
-        <v-list-item
-          v-for="n in 5"
-          :key="n"
-          link
-        >
-          <v-list-item-content>
-            <v-list-item-title>Item {{ n }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+  
 
     <v-main>
       <!--  -->
@@ -86,6 +27,7 @@
       height="72"
       inset
     >
+    <div max-width="300" class="center">
       <v-text-field
         background-color="grey lighten-1"
         dense
@@ -94,6 +36,7 @@
         rounded
         solo
       ></v-text-field>
+    </div>
     </v-footer>
   </v-app>
 </template>
@@ -103,3 +46,10 @@
     data: () => ({ drawer: null }),
   }
 </script>
+
+<style scoped>
+.center {
+  margin: auto;
+  width: 50%;
+}
+</style>
