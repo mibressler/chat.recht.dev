f7927a8b8949b6a8ca47024f63c04d39e0a17988
Till Müller <tillhendrikmueller@gmail.com>
2023-01-06 09:13:01 +0100

    Add Aleph Alpha to the backend

--- rechtgpt/backend-api/main.py
+++ rechtgpt/backend-api/main.py
@@ -9,6 +9,7 @@ import html
 import re
 import json
 from fastapi.middleware.cors import CORSMiddleware
+from aleph_alpha_client import AlephAlphaModel, SemanticEmbeddingRequest, SemanticRepresentation, Prompt
 
 # TODO AI-generated summaries (plus have them in the DB)
 
@@ -48,14 +49,28 @@ app.add_middleware(CORSMiddleware,
 with open('.apikey', 'r') as f:
     lines = f.readlines()
     openai.api_key = lines[0].strip()
-    pinecone.init(api_key=lines[1].strip())
-    index = pinecone.Index('2019-02-19-oldp-cases')
+    pinecone_apikey_openai = lines[1].strip()
+    aam = AlephAlphaModel.from_model_name("luminous-base", lines[2].strip())
+    pinecone_apikey_alephalpha = lines[3].strip()
 
 @app.get("/reference")
-async def get_reference(prompt: str, db: Session = Depends(get_db)):
-    embedding = openai.Embedding.create(input = prompt, model="text-embedding-ada-002")
-    print(embedding['usage'])
-    res = index.query(vector = embedding['data'][0]['embedding'], namespace = 'content', top_k = 10, include_metadata = True)
+async def get_reference(prompt: str, engine: int = 0, db: Session = Depends(get_db)):
+    if engine == 0:
+        pinecone.init(api_key=pinecone_apikey_openai)
+        index = pinecone.Index('2019-02-19-oldp-cases')
+        embedding = openai.Embedding.create(input = prompt, model="text-embedding-ada-002")
+        print(embedding['usage'])
+        res = index.query(vector = embedding['data'][0]['embedding'], namespace = 'content', top_k = 10, include_metadata = True)
+    elif engine == 1:
+        pinecone.init(api_key=pinecone_apikey_alephalpha)
+        index = pinecone.Index('aleph-alpha')
+        request = SemanticEmbeddingRequest(prompt=Prompt.from_text(prompt), representation=SemanticRepresentation.Query)
+        embedding = aam.semantic_embed(request).embedding
+        res = index.query(vector = embedding, namespace = 'content', top_k = 10, include_metadata = True)
+        for elem in res['matches']:
+            elem['id'] = elem['metadata']['id']
+    else:
+        raise HTTPException(status_code=400, detail="Invalid request")
 
     response_data = []
     for elem in res['matches']:
@@ -71,6 +86,7 @@ async def get_reference(prompt: str, db: Session = Depends(get_db)):
         })
     return {
         "prompt": prompt,
+        "engine": engine,
         "data": response_data
     }
 
